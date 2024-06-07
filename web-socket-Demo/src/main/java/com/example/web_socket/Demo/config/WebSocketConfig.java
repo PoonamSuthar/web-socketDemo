d0951aaf28e102bcdbc8c4b57e96157ab8dee39e
PoonamSuthar <sutha.4@iitj.ac.in>
2024-06-07 11:46:50 +0530

    Initial commit

---
+++ web-socket-Demo/src/main/java/com/example/web_socket/Demo/config/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.example.web_socket.Demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
